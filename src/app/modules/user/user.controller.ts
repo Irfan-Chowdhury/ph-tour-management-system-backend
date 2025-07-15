455a29045d8436f6dbb743b14d95e16a0f10647b
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-07-15 20:34:36 +0600

    26-9 Avoid Repetition of Try-Catch, use catchAsync

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,35 +3,80 @@ import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import { UserService } from "./user.service";
 import AppError from "../../errorHelpers/AppError";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {    
-        // throw new Error("fake error");
-        // throw new AppError(httpStatus.BAD_REQUEST, "fake error");
 
-        const user = await UserService.createUser(req.body);
+// const createUserFunction = async (req: Response, res: Response) => {
+//     const user = await UserService.createUser(req.body);
 
-        res.status(httpStatus.CREATED).json({
-            message: "User Created Successfully",
-            user
-        })
-    } catch (error:any) {
-        console.log(error);
+//     res.status(httpStatus.CREATED).json({
+//         message: "User Created Successfully",
+//         user
+//     })
+// }
 
-        // 1st Approch
-        // res.status(httpStatus.BAD_REQUEST).json({
-        //     message: `Something went worng !! ${error.message} From UserController`,
-        //     error
-        // })
+// type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
 
-        // 2nd Approch
-        next(error);
+// const catchAsync = (fn : AsyncHandler) => (req: Request, res:Response, next:NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((error:any) => {
+//         console.log(error);
+//         next(error);
+//     });
+// }
 
-    }
-}
+
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {    
+//         // throw new Error("fake error");
+//         // throw new AppError(httpStatus.BAD_REQUEST, "fake error");
+
+//         const user = await UserService.createUser(req.body);
+
+//         res.status(httpStatus.CREATED).json({
+//             message: "User Created Successfully",
+//             user
+//         })
+//     } catch (error:any) {
+//         console.log(error);
+
+//         // 1st Approch
+//         // res.status(httpStatus.BAD_REQUEST).json({
+//         //     message: `Something went worng !! ${error.message} From UserController`,
+//         //     error
+//         // })
+
+//         // 2nd Approch
+//         next(error);
+//     }
+// }
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserService.createUser(req.body);
+
+    res.status(httpStatus.CREATED).json({
+        message: "User Created Successfully",
+        user
+    });
+});
+
+
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserService.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+        success:true,
+        message: "All Users Retrieved Successfully",
+        data:users
+    });
+});
+
+
+
+
 
 export const UserController = {
-    createUser
+    createUser,
+    getAllUsers
 }
 
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -12,6 +12,15 @@ const createUser =  async (payload: Partial<IUser>) => {
     return user;
 }
 
+
+const getAllUsers = async () => {
+    const users = await User.find({});
+    return users;
+};
+
+
+
 export const UserService = {
-    createUser
+    createUser,
+    getAllUsers
 }

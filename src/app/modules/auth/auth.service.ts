7bd12ba3f4dcbc4e760c3484720cd7ab6d07b667
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-07-23 18:45:22 +0600

    28-6 Implement Reset / Change Password

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import { envVars } from "../../config/env";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { generateToken } from "../../utils/jwt";
 import { createNewAccessTokenWithRefreshToken, createUserTokens } from "../../utils/userTokens";
 
@@ -55,10 +55,24 @@ const getNewAccessToken = async (refreshToken: string) => {
     return {
         accessToken: newAccessToken
     }
+}
 
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+
+    const user = await User.findById(decodedToken.userId);
+
+    const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user!.password as string);
+    if (!isOldPasswordMatch) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "Old Password does not match");
+    }
+
+    user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUND));
+
+    user!.save();
 }
 
 export const AuthServices = {
     credentialsLogin,
-    getNewAccessToken
+    getNewAccessToken,
+    resetPassword
 }

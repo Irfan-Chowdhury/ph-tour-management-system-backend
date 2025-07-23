1610edee893065e526bdb88ed6d1dfc958566fc1
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-07-24 01:08:02 +0600

    28-8 Configuring Passport JS for Backend and Google Authentication Configuration

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,18 @@ import httpStatus from "http-status-codes";
 import { success } from "zod";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
-
+import passport from "passport";
+import expressSession from "express-session";
 
 const app =  express();
 
+app.use(expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());

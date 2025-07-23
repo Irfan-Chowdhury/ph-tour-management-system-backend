63ee572f75936ad3c22ccb25b9317ffe3621c210
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-07-23 15:21:17 +0600

    28-4 Create utility functions for getting accessToken and refreshToken and setting browser cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,23 +5,23 @@ import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import { AuthServices } from "./auth.service"
 import AppError from "../../errorHelpers/AppError"
-// import { setAuthCookie } from "../../utils/setCookie"
+import { setAuthCookie } from "../../utils/setCookie"
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    res.cookie("refreshToken", loginInfo.refreshToken, {
-        httpOnly: true,
-        secure: false,
-    });
+    // res.cookie("refreshToken", loginInfo.refreshToken, {
+    //     httpOnly: true,
+    //     secure: false,
+    // });
 
-    res.cookie("accessToken", loginInfo.accessToken, {
-        httpOnly: true,
-        secure: false
-    });
+    // res.cookie("accessToken", loginInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // });
 
 
-    // setAuthCookie(res, loginInfo);
+    setAuthCookie(res, loginInfo);
 
     sendResponse(res, {
         success: true,
@@ -45,7 +45,7 @@ const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: N
     //     secure: false
     // })
 
-    // setAuthCookie(res, tokenInfo);
+    setAuthCookie(res, tokenInfo);
 
     sendResponse(res, {
         success: true,
